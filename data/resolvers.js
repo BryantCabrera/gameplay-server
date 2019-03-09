503ee4649fa87410d846564711a1cf39de3faa99
Bryant <cabrera.bryant@gmail.com>
2019-03-08 21:19:33 -0800

    Adds bcrypt hashed password to resolvers.js > mutation > createUser.

--- data/resolvers.js
+++ data/resolvers.js
@@ -48,12 +48,8 @@ export const resolvers = {
     Mutation: {
         createUser: async (parent, args) => {
             //hashed password
-            // const password = args.input.password;
-            // const hashedPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-
             args.input.password = bcrypt.hashSync(args.input.password, bcrypt.genSaltSync(10));
 
-            // console.log(bcrypt.hashSync(args.input.password, bcrypt.genSaltSync(10)), ' this is args from resolvers.js > mutation > createUser');
             console.log(args, ' this is args from resolvers.js > mutation > createUser');
 
             try {
